56ef9005c7ec7bf94727fb3eebffa577a89ecf26
Suhas1320 <suhas00613@gmail.com>
2023-07-13 14:20:57 +0530

    adding garden file crude

---
+++ Amazon.java
@@ -0,0 +1,12 @@
+class Amazon{
+
+public static void login(String name,String password){
+if( password="123456g"){
+
+System.out.println("login succeafull");
+}
+else
+{
+System.out.println("invalid password retry");
+
+}
